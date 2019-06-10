7ce7e67f1c85e703e0b02fa571bb80d0ff198adc
zonagilreath <bryson.gilreath@gmail.com>
2019-06-10 12:23:18 -0500

    Complete react-components prompt

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // TODO
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => 
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+
+  render(){
+    const style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    }
+    return (
+      <li style={style} 
+          onClick={this.onListItemClick.bind(this)} 
+          onMouseEnter={this.onListItemHover.bind(this)} 
+          onMouseLeave={this.onListItemHover.bind(this)}>
+            { this.props.item }
+      </li>)
+  }
+}
+
+ReactDOM.render(<GroceryList items={['Bananas', 'Peanut butter']} />, document.getElementById('app'));
